f186d850b378efbb3f7fba328b9a981e4ccd405c
habardasher30 <nishit.batwal@gmail.com>
2024-04-04 19:03:08 +0400

    icons fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
       <div className="h-dvh w-screen flex flex-col items-center justify-center gap-10">
         <img src="/profile.png" alt="profile" className="w-96 h-auto" />
 
-        <p className="text-2xl font-semibold">You have been hacked by</p>
+        <p className="text-2xl md:text-3xl font-semibold">
+          You have been hacked by
+        </p>
 
         <h1 className="text-5xl md:text-8xl font-bold">NISHIT BATWAL</h1>
       </div>
