147647c89cc2fa9386ea10ec8679e2bdedf52b67
habardasher30 <nishit.batwal@gmail.com>
2024-04-04 18:31:48 +0400

    metadata

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 function App() {
   return (
     <React.Fragment>
-      <div className="h-dvh w-screen flex flex-col items-center justify-center">
-        <p>You have Landed</p>
+      <div className="h-dvh w-screen flex flex-col items-center justify-center gap-10">
+        <img src="/profile.png" alt="profile" className="w-96 h-auto" />
 
-        <h1 className="text-8xl font-bold">NISHIT BATWAL</h1>
+        <p className="text-2xl font-semibold">You have been hacked by</p>
+
+        <h1 className="text-5xl md:text-8xl font-bold">NISHIT BATWAL</h1>
       </div>
     </React.Fragment>
   );
