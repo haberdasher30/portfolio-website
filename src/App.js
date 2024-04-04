70a33e8fcd3e3120b5ee3604cd8696c4e63d63c9
habardasher30 <nishit.batwal@gmail.com>
2024-04-04 16:46:11 +0400

    tailwind config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+
 function App() {
   return (
-   <div>
-    NISHIT BATWAL
-   </div>
+    <React.Fragment>
+      <div className="h-dvh w-screen flex flex-col items-center justify-center">
+        <p>You have Landed</p>
+
+        <h1 className="text-8xl font-bold">NISHIT BATWAL</h1>
+      </div>
+    </React.Fragment>
   );
 }
 
