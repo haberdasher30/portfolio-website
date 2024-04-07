fe041b67fc517c135125116c817a8e187857d2b8
Archit1235 <f20210124@dubai.bits-pilani.ac.in>
2024-04-07 21:59:03 +0400

    mobile fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="w-full h-dvh bg-[url('/background.svg')] bg-contain bg-fixed flex items-center justify-center">
-      <div className="container flex flex-row items-center justify-center gap-5 w-full h-full">
+    <div className="w-full h-dvh bg-[url('/background.svg')] bg-contain bg-center bg-fixed flex items-center justify-center">
+      <div className="container flex flex-col-reverse md:flex-row items-center justify-center gap-5 w-full h-full">
         <Image
           width={1988}
           height={3740}
           src="/funko.png"
-          className="w-auto h-[70%] bg-transparent"
+          className="w-auto h-[40%] md:h-[70%] bg-transparent"
           priority
         />
 
-        <div className="flex flex-col items-center justify-center w-full h-full">
-          <h1 className="relative w-[max-content] text-6xl font-bold before:absolute before:inset-0 before:animate-typewriter before:bg-backgroundRoot after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-whiteColor text-greenColor">
+        <div className="flex flex-col items-center justify-center w-full">
+          <h1 className="relative w-[max-content] text-3xl md:text-6xl font-bold before:absolute before:inset-0 before:animate-typewriter before:bg-backgroundRoot after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-whiteColor text-greenColor">
             Hi, I am Nishit Batwal..
           </h1>
         </div>
