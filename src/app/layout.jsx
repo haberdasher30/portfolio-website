94ebc17f881d11bc363b189efc38ba4bf213b614
Archit1235 <f20210124@dubai.bits-pilani.ac.in>
2024-09-21 20:23:03 +0400

    seo and package updates

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,10 +5,60 @@ import { Analytics } from '@vercel/analytics/react';
 import NavBar from '@/components/shared/NavBar';
 import { Providers } from '@/components/shared/Provider';
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#141d2b',
+};
+
 export const metadata = {
-  title: 'Portfolio | Nishit Batwal',
+  title: 'Nishit Batwal | Portfolio',
   description: "Nishit Batwal's portfolio",
   image: '/logo.jpeg',
+  metadataBase: new URL('https://www.nishitbatwal.com/'),
+  openGraph: {
+    siteName: 'Nishit | Portfolio',
+    type: 'website',
+    locale: 'en_US',
+    image: '/logo.jpeg',
+  },
+  applicationName: 'Nishit Batwal | Portfolio',
+  appleWebApp: {
+    title: 'Nishit Batwal | Portfolio',
+    statusBarStyle: 'default',
+    capable: true,
+  },
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        type: 'image/x-icon',
+      },
+      {
+        url: '/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+    ],
+    shortcut: [
+      {
+        url: '/favicon.ico',
+        type: 'image/x-icon',
+      },
+    ],
+    apple: [
+      {
+        url: '/apple-icon-57x57.png',
+        sizes: '57x57',
+        type: 'image/png',
+      },
+      {
+        url: '/apple-icon-60x60.png',
+        sizes: '60x60',
+        type: 'image/png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,34 @@ import Videos from '@/components/home/Videos';
 import PortfolioTabs from '@/components/home/PortfolioTabs';
 import Introduction from '@/components/home/Introduction';
 
+export const metadata = {
+  title: 'Home | Nishit Batwal',
+  description: 'Home Nishit Batwal',
+  keywords: [
+    'portfolio',
+    'nishit batwal',
+    'cybersecurity',
+    'gdsc',
+    'gdg',
+    'developer student clubs',
+    'google developer groups',
+    'google',
+    'developer',
+    'student',
+    'clubs',
+    'groups',
+    'hack the box',
+    'hackthebox',
+  ],
+  openGraph: {
+    url: 'https://www.nishitbatwal.com/',
+    type: 'website',
+    title: 'Home | Nishit Batwal',
+    description: 'Home Nishit Batwal',
+    image: '/logo.jpeg',
+  },
+};
+
 export default function Home() {
   return (
     <div className='flex flex-col items-center gap-10 md:gap-20 mb-10 md:mb-20 w-full h-full'>
